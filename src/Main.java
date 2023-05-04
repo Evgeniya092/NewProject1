7397d99affd069414148e5fcd16bc1a17b641e2a
Evgeniya <ederendyaeva92@gmail.com>
2023-05-05 00:26:14 +0500

    Added NewProject1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 14580;
+        int b = 20;
+
+        int c = a / b;
+        System.out.println("Бонусных миль начислено:" + c);
+    }
+}
