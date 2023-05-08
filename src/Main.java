51c35ac4210db341f6119063035ddf7229e6e8e9
Evgeniya <ederendyaeva92@gmail.com>
2023-05-08 18:26:14 +0500

    Added fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 14580;
-        int b = 20;
+        int price = 14580;
+        int bonusMiles = 20;
 
-        int c = a / b;
-        System.out.println("Бонусных миль начислено:" + c);
+        int miles = price / bonusMiles;
+        System.out.println("Бонусных миль начислено:" + miles);
     }
 }
